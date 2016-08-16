32c327cb401308bf66fefbea5037ed37245db287
ReneWi <rene.wiskow@web.de>
2016-08-16 11:54:39 +0200

    Solved

--- 03Serialisierung/XML/SerializableObject.java
+++ 03Serialisierung/XML/SerializableObject.java
@@ -8,13 +8,15 @@ public class SerializableObject<T> implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private int hiddenhInt = 42;	// <---- funktioniert noch nicht müssen rausfinden wie !!
+	private int hiddenhInt;	
 	private T value;
 	
 	public SerializableObject(){} // <---- Dont you ever forget this shit !
 	
 	public SerializableObject(T value){
 		this.value = value;
+		this.hiddenhInt = 42;	//<---- @Transient funktioniert wenn Attribut in Objekt implementiert wird, ansonsten sollte man es
+					// 	static machen !
 	}
 	public T getVal(){
 		return value;
