51d2093062468650404a4131f177f37095a12a61
ReneWi <rene.wiskow@web.de>
2016-08-17 11:36:28 +0200

    Add files via upload

--- 05Threads/ThreadGroups/Main.java
+++ 05Threads/ThreadGroups/Main.java
@@ -1,1 +1,19 @@
+package treadGroups;
 
+public class Main {
+	public static void main(String[] args) {
+		ThreadGroup group = new ThreadGroup("Threadson Five");
+		RunnableThread rt1 = new RunnableThread("Hee hee ");
+		RunnableThread rt2 = new RunnableThread("Shamona!");
+		Thread t1 = new Thread(group, rt1);
+		Thread t2 = new Thread(group, rt2);
+		t1.start();
+		System.out.println("t1 : " + t1.getThreadGroup());
+		t2.start();
+		System.out.println("This Thread: " + Thread.currentThread().getThreadGroup());
+		System.out.println("t1 : " + t1.getThreadGroup());
+		while(group.activeCount()>0){
+			System.out.println("blub");
+		}
+	}
+}
---
+++ 05Threads/ThreadGroups/RunnableThread.java
@@ -0,0 +1,15 @@
+package treadGroups;
+
+public class RunnableThread implements Runnable{
+	private String s;
+	public RunnableThread(String s){
+		this.s = s;
+	}
+	@Override
+	public void run() {
+		for(int i = 0; i<s.length();i++){
+			System.out.println(s.charAt(i));
+		}
+	}
+
+}
