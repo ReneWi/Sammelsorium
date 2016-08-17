cc3b3fb0a848a6bb96a05dca16649d87925abab2
ReneWi <rene.wiskow@web.de>
2016-08-17 11:01:42 +0200

    Update Main.java

--- 05Threads/Runnable/Main.java
+++ 05Threads/Runnable/Main.java
@@ -5,12 +5,13 @@ public static void main(String[] args) {
 	RunnableThread t = new RunnableThread("Your ad could be here!");
 	Thread thread = new Thread(t);
 	thread.start();
+	System.out.println("Im not waiting for that mofo!");
 	try {
 		thread.join();
 	} catch (InterruptedException e) {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
 	}
-	System.out.println("ich mach auch was ");
+	System.out.println("I am!");
 }
 }
