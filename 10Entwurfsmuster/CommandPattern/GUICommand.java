a9803ca7475ebed1a738b5b058683d2dd268535f
ReneWi <rene.wiskow@web.de>
2016-08-18 12:47:09 +0200

    Add files via upload

--- 10Entwurfsmuster/CommandPattern/GUICommand.java
+++ 10Entwurfsmuster/CommandPattern/GUICommand.java
@@ -1,1 +1,40 @@
+package commandPattern;
 
+public class GUICommand {
+	void Execute(){}
+}
+class PasteCommand extends GUICommand{
+	Document document;
+	void Execute(){
+		document.Paste();
+	}
+}
+class MenuItem{
+	GUICommand command;
+	void DefaultInit(){
+		command = new GUICommand();
+	}
+	void Clicked(){
+		if(clicked){
+			command.Execute();
+		}
+	}
+}
+class Application{
+	MenuItem mi;
+	PasteCommand pc;
+	boolean real_use;
+	
+	void StoreCommand(GUICommand c){
+		mi.command = c;
+	}
+	void AddMenuItem(){
+		mi = new MenuItem();
+		if(real_use){
+			pc = new PasteCommand();
+			StoreCommand(pc);
+		}else{
+			mi.DefaultInit();
+		}
+	}
+}
