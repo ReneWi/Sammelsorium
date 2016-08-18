e6293053520fab517b3434765a37604450fa27a3
ReneWi <rene.wiskow@web.de>
2016-08-18 11:56:58 +0200

    pseudocode

--- 10Entwurfsmuster/ProxyPattern/VirtualProxy/ProxyImage.java
+++ 10Entwurfsmuster/ProxyPattern/VirtualProxy/ProxyImage.java
@@ -1,1 +1,29 @@
+package virtualProxy;
 
+
+public class ProxyImage {
+private Image image;
+private String url;
+private int sizeX;
+private int sizeY;
+
+public ProxyImage(String url, int sizeX, int sizeY){
+	this.image = new Image();
+	this.url = url;
+	this.sizeX = sizeX;
+	this.sizeY = sizeY;
+	startAsyncImageLoadingInThread();
+}
+void startAsyncImageLoadingInThread(){
+	image.loadImageFromUrl(url);
+}
+void showImage(){
+	if(image.getIsLoaded()){
+		//Echtes Bild
+		canvas.drawImage(image.getImageData());
+	}else{
+		//Rechteck als Stellvertreter
+		canvas.drawRectangle(sizeX,sizeY);
+	}
+}
+}
