fa0fc61af55c3d65a6ff4695bf917622065d0dc4
ReneWi <rene.wiskow@web.de>
2016-08-18 12:08:22 +0200

    pseudocode

--- 10Entwurfsmuster/ProxyPattern/ProtectionProxy/RealRobot.java
+++ 10Entwurfsmuster/ProxyPattern/ProtectionProxy/RealRobot.java
@@ -1,1 +1,23 @@
+package protectionPattern;
 
+public interface RealRobot {
+void drive(double deltaPos);
+double getCurrentPosition();
+}
+class ProxyRobot{
+	private RealRobot realRobot = new RealRobot();
+	private double maxPosition = 100.00;
+	private double minPosition = 0.00;
+	
+	boolean driveSafely(double deltaPos){
+		double currentPosition = realRobot.getCurrentPosition();
+		double desiredPosition = currentPosition + deltaPos;
+		if(desiredPosition <= maxPosition && desiredPosition >= minPosition ){
+			robot.drive(deltaPos);
+			return true;
+		}else{
+			System.out.println("Illegal Movement requested!");
+			return false;
+		}
+	}
+}
