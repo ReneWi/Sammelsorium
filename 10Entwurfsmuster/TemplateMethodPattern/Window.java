a2c566307d1081bf961601166ac8fc3e56ad8975
ReneWi <rene.wiskow@web.de>
2016-08-18 12:21:07 +0200

    Add files via upload

--- 10Entwurfsmuster/TemplateMethodPattern/Window.java
+++ 10Entwurfsmuster/TemplateMethodPattern/Window.java
@@ -1,1 +1,20 @@
+package templateMethodPattern;
 
+public abstract class Window {
+	void display(){
+		SetFocus();
+		DoDisplay();
+	}
+	void SetFocus(){}
+	abstract void DoDisplay();
+}
+
+class MyWindow extends Window{
+
+	@Override
+	void DoDisplay() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
